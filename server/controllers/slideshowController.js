66d97b2aea73c5306866e63d681d68e9a8c5a397
Gregor Labadan <gregorlorenzolabadan@gmail.com>
2024-09-20 23:49:09 +0800

    removed console logs

--- server/controllers/slideshowController.js
+++ server/controllers/slideshowController.js
@@ -1,5 +1,4 @@
 const Slideshow = require('../models/Slideshow');
-const mongoose = require('mongoose');
 
 exports.getSlideshow = async (req, res) => {
   try {
@@ -15,17 +14,13 @@ exports.getSlideshow = async (req, res) => {
 
 exports.createSlideshow = async (req, res) => {
   try {
-    console.log('Received request body:', req.body);
-
     const existingSlideshow = await Slideshow.findOne();
     if (existingSlideshow) {
-      console.log('Slideshow already exists');
       return res.status(400).json({ message: 'Slideshow already exists' });
     }
 
     const { slides } = req.body;
     if (!slides || !Array.isArray(slides)) {
-      console.log('Invalid slides data');
       return res.status(400).json({ message: 'Invalid slides data' });
     }
 
@@ -36,16 +31,11 @@ exports.createSlideshow = async (req, res) => {
       }))
     });
 
-    console.log('Slideshow to be saved:', slideshow);
-
     await slideshow.save();
 
     const createdSlideshow = await Slideshow.findById(slideshow._id).populate('slides.slide');
-    console.log('Created slideshow:', createdSlideshow);
-
     res.status(201).json(createdSlideshow);
   } catch (error) {
-    console.error('Error creating slideshow:', error);
     res.status(400).json({
       message: 'Error creating slideshow',
       error: error.message,
@@ -56,7 +46,6 @@ exports.createSlideshow = async (req, res) => {
 
 exports.updateSlideshow = async (req, res) => {
   try {
-    console.log('Updating slideshow. Request body:', req.body);
     const { slides } = req.body;
 
     if (!slides || !Array.isArray(slides)) {
@@ -66,33 +55,22 @@ exports.updateSlideshow = async (req, res) => {
     let slideshow = await Slideshow.findOne();
 
     if (!slideshow) {
-      console.log('No slideshow found. Creating a new one.');
       slideshow = new Slideshow();
     }
-
-    console.log('Slideshow before update:', slideshow);
 
     slideshow.slides = slides.map((slide, index) => ({
       slide: slide.id,
       position: index + 1
     }));
 
-    console.log('Updated slideshow before save:', slideshow);
-
     await slideshow.save();
 
     const updatedSlideshow = await Slideshow.findById(slideshow._id).populate('slides.slide');
-    console.log('Updated/Created slideshow after save:', updatedSlideshow);
-
     res.json(updatedSlideshow);
   } catch (error) {
-    console.error('Error updating/creating slideshow:', error);
-
     if (error.name === 'ValidationError') {
       return res.status(400).json({ message: 'Validation Error', details: error.errors });
     }
-
-    console.error('Full error stack:', error.stack);
 
     res.status(500).json({
       message: 'Internal Server Error',
